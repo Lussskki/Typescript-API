395fdfd1be01483c731d853819e6f98e65a1cfec
Luka <lussskki@gmail.com>
2026-02-28 14:25:59 +0400

    feat: add profile route which testes payload of database  and see user json when you connect to profile

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
+import type { JwtPayload } from "jsonwebtoken";
+
 import { loginController } from "../controllers/loginController";
 import { signupController } from "../controllers/signupController";
 import { authMiddleware } from "../middlewares/jwt";
+import { User } from "../models/user"
 
 const router = Router();
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
-router.get("/profile", authMiddleware, (req, res) => {
-  res.json({ message: "Protected works" });
+router.get("/profile", authMiddleware, async (req, res) => {
+  const payload = req.user as JwtPayload;
+
+  const user = await User.findById(payload.userId).select("-password");
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  return res.json(user);
 });
 
 export default router;
